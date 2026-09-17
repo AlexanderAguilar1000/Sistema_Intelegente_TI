420ac8221806646717da2a6d5d0f72e67bc8acdf
Alexander <cresistem99@gmail.com>
2026-09-17 12:47:58 -0500

    Agregando backend

---
+++ SistemaInteligenteTI/src/main/java/com/corporativoTI/SistemaInteligenteTI/SistemaInteligenteTiApplication.java
@@ -0,0 +1,13 @@
+package com.corporativoTI.SistemaInteligenteTI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInteligenteTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaInteligenteTiApplication.class, args);
+	}
+
+}
---
+++ SistemaInteligenteTI/src/test/java/com/corporativoTI/SistemaInteligenteTI/SistemaInteligenteTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporativoTI.SistemaInteligenteTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInteligenteTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
